0b555fec84e733f11d1a871f2de2e7e96a0b40a3
artze <arthurwtm@gmail.com>
2019-01-19 17:36:19 +0100

    Write test for textComplexityApi

---
+++ test/textComplexityApi.spec.js
@@ -0,0 +1,44 @@
+const expect = require('chai').expect;
+const request = require('request');
+const complexityApiUrl = 'http://localhost:3000/complexity'
+
+describe('Text complexity API', function() {
+    describe('Query text complexity by calling API endpoint', function() {
+        it('Should return correct values in correct format', function(done) {
+            const requestBody = {
+                textInput: 'Kim loves going​ to the cinema'
+            }
+
+            request.post({
+                url: complexityApiUrl,
+                body: JSON.stringify(requestBody)
+            }, function(err, res, body) {
+                expect(JSON.parse(body)).to.deep.equal({
+                    data: {
+                        overall_ld: 0.67
+                    }
+                })
+            })
+        })
+    })
+
+    describe('Query text complexity by calling API endpoint in \'verbose\' mode', function() {
+        it('Should return correct values in correct format', function() {
+            const requestBody = {
+                textInput: 'Kim loves going​ to the cinema. A quick brown fox jumps over the lazy dog.'
+            }
+
+            request.post({
+                url: complexityApiUrl + '?mode=verbose',
+                body: JSON.stringify(requestBody)
+            }, function(err, res, body) {
+                expect(JSON.parse(body)).to.deep.equal({
+                    data: {
+                        sentence_ld: [0.67, 0.78],
+                        overall_ld: 0.73
+                    }
+                })
+            })
+        })
+    })
+})
