f2e1572ac723e8a1213d4b85be50141741864e27
artze <arthurwtm@gmail.com>
2019-01-19 21:19:07 +0100

    Handle incorrect API enpoint url to return 404 ResourceNotFound

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const textComplexityRoutes = require('./textComplexityRoutes');
+const applicationController = require('../controllers/application');
 const errorHandler = require('../middleware/errorHandler');
 
 function init(app) {
 
     app.use('/complexity', textComplexityRoutes);
     
+    app.all('*', applicationController.handleResourceNotFound);
+
     app.use(errorHandler);
 
 }
