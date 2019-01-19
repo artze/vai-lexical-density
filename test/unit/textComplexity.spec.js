c6a28526d544a18a004926ab4299add732b71f4d
artze <arthurwtm@gmail.com>
2019-01-19 15:43:41 +0100

    Create external function to normalize text input during textComplexity computation

--- test/unit/textComplexity.spec.js
+++ test/unit/textComplexity.spec.js
@@ -2,7 +2,6 @@ const expect = require('chai').expect;
 const analyzeText = require('../../lib/analyzeText');
 
 describe('Text complexity computation', function() {
-
     describe('Compute overall text complexity', function() {
         it('should return correct complexity result', function() {
             const complexityResult = analyzeText.computeOverallLexicalDensity('Kim loves going​ to the cinema')
@@ -10,12 +9,10 @@ describe('Text complexity computation', function() {
         })
     })
     
-    
     describe('Compute text complexity of each sentence', function() {
         it('should return complexity result', function() {
             const complexityResult = analyzeText.computeLexicalDensityOfEachSentence('Kim loves going​ to the cinema. A quick brown fox jumps over the lazy dog.')
             expect(complexityResult).to.deep.equal([0.67, 0.78]);
         })
     })
-    
 })
