b642a5ede04ca1b73c4b839e833e7ccd6327ef2c
artze <arthurwtm@gmail.com>
2019-01-19 10:34:26 +0100

    Remove un-needed lines from app.js boilerplate

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
