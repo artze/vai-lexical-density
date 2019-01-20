a4fa1e47e196de02a481075a42937a014f7adb3a
artze <arthurwtm@gmail.com>
2019-01-20 16:39:33 +0100

    Add auth check in controller

--- controllers/nonLexicalWords.js
+++ controllers/nonLexicalWords.js
@@ -1,11 +1,25 @@
 // @ts-check
+const basicAuth = require('basic-auth');
 const dbService = require('../services/dbService');
+const authService = require('../services/authService');
 const InvalidInputError = require('../errors/InvalidInputError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 function addNonLexicalWordToDb(req, res, next) {
     // auth check
+    let credentials = basicAuth(req);
+
+    try {
+        if(!credentials || !authService.checkCredentials(credentials.name, credentials.pass)) {
+            throw new UnauthorizedError('Access Denied due to invalid credentials');
+        }
+    } catch (err) {
+        next(err);
+        return;
+    }
     
     const newWord = req.body.wordInput;
+
     // Validate empty request body
     try {
         if (!newWord) {
