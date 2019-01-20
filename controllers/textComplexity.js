699ee984deceff73b0ef34cbdca1178170bd5f2f
artze <arthurwtm@gmail.com>
2019-01-20 13:57:20 +0100

    Retrieve non-lexical words with db service before computing complexity

--- controllers/textComplexity.js
+++ controllers/textComplexity.js
@@ -2,6 +2,7 @@
 
 const analyzeText = require('../lib/analyzeText');
 const validate = require('../lib/validate');
+const dbService = require('../services/dbService');
 const InvalidInputError = require('../errors/InvalidInputError');
 const InputLengthExceededError = require('../errors/InputLengthExceededError');
 
@@ -28,30 +29,39 @@ function computeTextComplexity(req, res, next) {
         return;
     }
     
-    // Handle valid input and respond with result
+    // Handle valid input
+    // Retrieve non-lexical words from db service
+    // respond to client with result
     const verboseMode = req.query.mode === 'verbose' ? true : false;
-
-    if (verboseMode) {
-        const sentence_ld = analyzeText.computeLexicalDensityOfEachSentence(textInput);
-        const overall_ld = analyzeText.computeOverallLexicalDensity(textInput);
-        const resultObj = {
-            data: {
-                sentence_ld: sentence_ld,
-                overall_ld: overall_ld
+    
+    dbService.getAllNonLexicalWords()
+        .then(function(nonLexicalWordsArr) {
+            if (verboseMode) {
+                const sentence_ld = analyzeText.computeLexicalDensityOfEachSentence(textInput, nonLexicalWordsArr);
+                const overall_ld = analyzeText.computeOverallLexicalDensity(textInput, nonLexicalWordsArr);
+                const resultObj = {
+                    data: {
+                        sentence_ld: sentence_ld,
+                        overall_ld: overall_ld
+                    }
+                }
+        
+                res.status(200).json(resultObj);
+            } else {
+                const overall_ld = analyzeText.computeOverallLexicalDensity(textInput, nonLexicalWordsArr);
+                const resultObj = {
+                    data: {
+                        overall_ld: overall_ld
+                    }
+                }
+        
+                res.status(200).json(resultObj);
             }
-        }
-
-        res.status(200).json(resultObj);
-    } else {
-        const overall_ld = analyzeText.computeOverallLexicalDensity(textInput);
-        const resultObj = {
-            data: {
-                overall_ld: overall_ld
-            }
-        }
-
-        res.status(200).json(resultObj);
-    }
+        })
+        .catch(function(err) {
+            next(err);
+            return;
+        })
 }
 
 module.exports = {
